03596f6cbb01a4114daad2043748044864b39167
Isaac Ayodeji <deejecks@live.com>
2014-12-23 05:59:42 +0100

    improved code quality

--- app/scripts/myScripts/controller.js
+++ app/scripts/myScripts/controller.js
@@ -6,16 +6,16 @@ $('table').hide();
 var appModule = angular.module('appModule', []);
 
 // DEFINITION REQUEST FACTORY
-appModule.factory('sendDefinitionRequest', ['$http', function(http) {
+appModule.factory('sendDefinitionRequest', ['$http', function (http) {
 
-  var returnDefinitionUrl = function (query) { 
+  var returnDefinitionUrl = function (query) {
     console.log(query);
-    var definitionUrl = 'http://api.wordnik.com:80/v4/word.json/'+ query + '/definitions/';
+    var definitionUrl = 'http://api.wordnik.com:80/v4/word.json/' + query + '/definitions/';
     console.log(definitionUrl);
     return definitionUrl;
   };
 
-  var requestFactory = function(query) {
+  var requestFactory = function (query) {
     return http({
       url: returnDefinitionUrl(query),
       method: 'GET',
@@ -26,22 +26,21 @@ appModule.factory('sendDefinitionRequest', ['$http', function(http) {
         api_key: '724583fdf72680c36d0010ad78b03b1c4f3ea7b27c651f094'
       }
     });
-    
   };
   return requestFactory;
 }]);
 
 // THESAURUS REQUEST FACTORY
-appModule.factory('sendThesaurusRequest', ['$http', function(http) {
+appModule.factory('sendThesaurusRequest', ['$http', function (http) {
 
-  var returnThesaurusUrl = function (query) { 
+  var returnThesaurusUrl = function (query) {
     console.log(query);
     var thesaurusUrl = 'http://words.bighugelabs.com/api/2/f5c79d25e5e8f8723ee62b695a8a7987/' + query + '/json';
     console.log(thesaurusUrl);
     return thesaurusUrl;
   };
 
-  var requestFactory = function(query) {
+  var requestFactory = function (query) {
     return http({
       url: returnThesaurusUrl(query),
       method: 'GET'
@@ -52,30 +51,30 @@ appModule.factory('sendThesaurusRequest', ['$http', function(http) {
 
 
 // APPLICATION CONTROLLER 
-appModule.controller('definitionAndThesaurus', ['$scope', 'sendDefinitionRequest', 'sendThesaurusRequest', function(scope, sendDefinitionRequest, sendThesaurusRequest) {
+appModule.controller('definitionAndThesaurus', ['$scope', 'sendDefinitionRequest', 'sendThesaurusRequest', function (scope, sendDefinitionRequest, sendThesaurusRequest) {
 
     // THE DEFINITION FUNCTION
-    scope.definitionSearch = function() {
+  scope.definitionSearch = function () {
 
-      sendDefinitionRequest(angular.lowercase(scope.query)).
-      success(function(response) {
-        scope.wordResponses = response;
-        $('img').hide();
-        $('.Definition').show();
-        $('.Thesaurus').show();
+    sendDefinitionRequest(angular.lowercase(scope.query)).
+    success(function (response) {
+      scope.wordResponses = response;
+      $('img').hide();
+      $('.Definition').show();
+      $('.Thesaurus').show();
     }).
-      error(function(data, status, headers, config) {
-        $('.responseAlert').show();
-      });;
-    };
+    error(function (data, status) {
+      $('.responseAlert').show();
+    });
+  }; // ENDS scope.definitionSearch
 
     // THE THESAURUS FUNCTION
-    scope.thesaurusSearch = function() {
-      sendThesaurusRequest(scope.query).success(function(response) {
+  scope.thesaurusSearch = function () {
+    sendThesaurusRequest(scope.query).success(function (response) {
 
-        scope.thesaurus = response;
-        $('img').hide();
-        $('.responseAlert').hide();
+      scope.thesaurus = response;
+      $('img').hide();
+      $('.responseAlert').hide();
 
       // DETERMINES THE PART OF SPEECH A WORD BELONGS TO
       // ITERATION OVER KEYS AND VALUES IN RESPONSE -- SCOPE.THESAURUS
@@ -86,41 +85,34 @@ appModule.controller('definitionAndThesaurus', ['$scope', 'sendDefinitionRequest
           angular.forEach(scope.nounThesaurus, function (value, key) {
             if (key == 'syn') {
               scope.nounSynonym = value;
-            }
-            else {
+            } else {
               scope.nounAntonym = value;
             }
           });
-        }
-        else if (key == 'verb') {
+        } else if (key == 'verb') {
           scope.verbThesaurus = value;
-          angular.forEach(scope.verbThesaurus, function(value, key) {
+          angular.forEach(scope.verbThesaurus, function (value, key) {
             if (key == 'syn') {
               scope.verbSynonym = value;
-            }
-            else {
+            } else {
               scope.verbAntonym = value;
             }
           });
-        }
-        else if (key == 'adverb') {
+        } else if (key == 'adverb') {
           scope.adverbThesaurus = value;
-          angular.forEach(scope.adverbThesaurus, function(value, key) {
+          angular.forEach(scope.adverbThesaurus, function (value, key) {
             if (key == 'syn') {
               scope.adverbSynonym = value;
-            }
-            else {
+            } else {
               scope.adverbAntonym = value;
             }
           });
-        }
-        else if (key == 'adjective') {
+        } else if (key == 'adjective') {
           scope.adjectiveThesaurus = value;
-          angular.forEach(scope.adjectiveThesaurus, function(value, key) {
+          angular.forEach(scope.adjectiveThesaurus, function (value, key) {
             if (key == 'syn' || key == 'rel') {
               scope.adjectiveSynonym = value;
-            }
-            else if (key == 'ant') {
+            } else if (key == 'ant') {
               scope.adjectiveAntonym = value;
             }
           });
@@ -152,15 +144,15 @@ appModule.controller('definitionAndThesaurus', ['$scope', 'sendDefinitionRequest
           $('.adjective').show();
         }
       });
-      }).error(function(data, status, headers, config) {
-        console.log(status);
-        $('table').hide();
-        $('.responseAlert').show();
-      });
-    };
+    }).error(function (data, status, headers, config) {
+      console.log(status);
+      $('table').hide();
+      $('.responseAlert').show();
+    });
+  };
 
     // TRIGGERS DEFINITION AND THESAURUS SEARCH ON THE CLICK OF ONE BUTTON
-    scope.search = function() {
+    scope.search = function () {
       if(scope.query.trim() !== '') {
         $('img').show();
         $('.alert').hide();
